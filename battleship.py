5b4fafb490071ae3c94b53d92f1d2924081f40b9
sg87018 <62720273+sg87018@users.noreply.github.com>
2023-07-17 09:33:03 -0400

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -10,25 +10,13 @@ def ran_col(grid):
 ship_row = ran_row(grid)
 ship_col = ran_col(grid)
 
-for turn in range(4):
-    print ("Turn"), turn
-    guess_row = int(input("Guess Row:"))
-    guess_col = int(input("Guess Col:"))
-
-    if guess_row == ship_row and guess_col == ship_col:
-        print("Congratulations! You sunk my battleship!")
-        break
-    else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print("Miss")
-        elif(grid[guess_row][guess_col] == "X"):
-            print("You guessed that one already.")
-        else:
-            print("You missed my battleship!")
-            grid[guess_row][guess_col] = "X"
+try:
+    for turn in range(4):
+        print ("Turn"), turn
     if turn == 4:
         print("Game Over")
     turn = turn + 1
-    print_grid(grid)
+except ValueError:
+    print(grid)
     
     
